df78031046fdeda413fe43428ac7b9c9a34f4047
Jasmine Anderson <jazz2900@gmail.com>
2018-12-15 16:57:29 -0800

    tested '/' server route and edited readme

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # user-api
 
 server running on port:4000
-
-api command:
-
+api setup:
 yarn dev
 
 
-server
-mongodb
-running on localhost: 27017
+database running on localhost: 27017
+database setup:
+step 1: run mongod
+step 2: run mongo
--- src/modules/users/user.controllers.js
+++ src/modules/users/user.controllers.js
@@ -3,7 +3,7 @@ import User from './user.model';
 export async function signUp(req, res) {
   try {
     const user = await User.create(req.body);
-    return res.status(201).json();
+    return res.status(201).json(user);
   } catch (e) {
     return res.status(500).json(e);
   }
