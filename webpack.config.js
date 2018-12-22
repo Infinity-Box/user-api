9447106af863133df934be2f9774eaa365b65d83
Jasmine Anderson <jazz2900@gmail.com>
2018-12-22 15:53:27 -0800

    reworked package.json

--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,32 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-     mode: 'production',
-     plugins: [
-       new CleanWebpackPlugin(['dist']),
-     externals: [nodeExternals()],
-     //This is where webpack will compile from
-     entry: {
-         'index': './src/index.js'
-     },
-     output: {
-         path: path.join(__dirname, 'dist'),
-         //name will be the entry filename
-         //in this example it 'index'
-         filename: '[name].bundle.js',
-         libraryTarget: 'commonjs2',
-     },
-     module: {
-         rules: [
-             {
-             test: /\.js$/,
-             //don't want webpack to compile the node folders
-             exclude: /node_modules/,
-             use: 'babel-loader'
-            }
-        ]
-     }
+  externals: [nodeExternals()],
+  //This is where webpack will compile from
+  entry: {
+      'index': './src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'])
+  ],
+   output: {
+       path: path.join(__dirname, 'dist'),
+       //name will be the entry filename
+       //in this example it 'index'
+       filename: '[name].bundle.js',
+       libraryTarget: 'commonjs2',
+   },
+   module: {
+       rules: [
+           {
+           test: /\.js$/,
+           //don't want webpack to compile the node folders
+           exclude: /node_modules/,
+           use: 'babel-loader',
+           // options: {
+           //  presets: ['babel/preset-env'],
+           // }
+          }
+      ]
+   }
 }
