2db27c91fe33b70af08aa8d77f0a314e378936f6
Jasmine Anderson <jazz2900@gmail.com>
2018-12-22 17:22:46 -0800

    test

--- src/index.js
+++ src/index.js
@@ -17,29 +17,18 @@ app.get('/', (req, res) => {
 
 apiRoutes(app);
 
-// let port = process.env.PORT
-//
-// app.listen(port, err => {
-//     if (err) {
-//         throw err;
-//     } else {
-//       console.log(`
-//         Server running on ${port}
-//         ---
-//         Running on ${process.env.NODE_ENV}
-//         ---
-//         Make something great
-//       `)
-//     }
-// })
+let port = process.env.PORT
 
-import http from 'http'
-
-// http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
-
-
-handle = (req, res) -> res.end "hit"
-
-server = http.createServer handle
-
-server.listen process.env.PORT || 5000
+app.listen(port, err => {
+    if (err) {
+        throw err;
+    } else {
+      console.log(`
+        Server running on ${port}
+        ---
+        Running on ${process.env.NODE_ENV}
+        ---
+        Make something great
+      `)
+    }
+})
