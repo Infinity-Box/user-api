7259a718160591872a3f6c5b7870dd8547109aea
Jasmine Anderson <jazz2900@gmail.com>
2018-12-22 17:13:05 -0800

    test

--- src/index.js
+++ src/index.js
@@ -38,6 +38,8 @@ import http from 'http'
 // http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
 
 
+handle = (req, res) -> res.end "hit"
+
 server = http.createServer handle
 
 server.listen process.env.PORT || 5000
